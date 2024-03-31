6f0a5fba5fec9da9d7b1f3a95dd7e7c06285553a
Hardik gupta <hardikgupta3003@gmail.com>
2024-03-31 15:23:07 +0530

    upadted

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,28 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { BACKEND_URL } from "../utils/http.js";
 
 
 export default function Card({video}){
     const [channel,setChannel] = useState({});
     const {status} = useSelector((state)=>state.auth)
+    const navigate = useNavigate()
     useEffect(()=>{
         (async () => {
             const res = await axios.get(`${BACKEND_URL}/users/find/${video.userId}`)
             setChannel(res.data)
         })()
     },[video.userId])
+
+    const handleCard = () => {
+        navigate(status ? `/video/${video._id}`:"/login")
+    }
     return (
-        <Link to={status?`/video/${video._id}`: "/login" } style={{textDecoration:"none"}}>
-        <div  className="w-360 mb-45 cursor-pointer">
+        // <Link to={status?`/video/${video._id}`: "/login" } style={{textDecoration:"none"}}>
+       
+        <div  onClick={handleCard} className="w-360 mb-45 cursor-pointer">
             <img className="w-full h-202 rounded-lg" src={video.thumbnail}/>
             <div className={`my-3 flex flex-wrap gap-2`}>
                 <img src={channel.avatar} className="w-9 h-9 border rounded-full bg-black" />
@@ -30,6 +36,7 @@ export default function Card({video}){
                 </div>     
             </div>
         </div>
-        </Link>
+       
+        // </Link>
     )
 }
