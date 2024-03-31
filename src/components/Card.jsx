a250a978d1286a0868d3d55e398ad78624ddb0e3
Hardik gupta <hardikgupta3003@gmail.com>
2024-03-31 20:18:46 +0530

    updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,12 +8,15 @@ import { BACKEND_URL } from "../utils/http.js";
 export default function Card({video}){
     const [channel,setChannel] = useState({});
     const {status} = useSelector((state)=>state.auth)
+    const [Loading,setLoading] = useState(false)
     const navigate = useNavigate()
     const num = +(video.duration)
     useEffect(()=>{
         (async () => {
+            setLoading(true)
             const res = await axios.get(`${BACKEND_URL}/users/find/${video.userId}`)
             setChannel(res.data)
+            setLoading(false)
         })()
     },[video.userId])
 
@@ -21,23 +24,26 @@ export default function Card({video}){
         navigate(status ? `/video/${video._id}`:"/login")
     }
     return (
-        // <Link to={status?`/video/${video._id}`: "/login" } style={{textDecoration:"none"}}>
-       
-        <div  onClick={handleCard} className="w-360 mb-45 cursor-pointer">
-            <img className="w-full h-202 rounded-lg" src={video.thumbnail}/>
-            <div className={`my-3 flex flex-wrap gap-2`}>
-                <img src={channel.avatar} className="w-9 h-9 border rounded-full bg-black" />
-                <p className="text-white">{video.title}</p>
+        <>
+       {
+        Loading ? (<div className="text-white">Loading ....</div>) : (
+
+            <div  onClick={handleCard} className="w-360 mb-45 cursor-pointer">
+                <img className="w-full h-202 rounded-lg" src={video.thumbnail}/>
+                <div className={`my-3 flex flex-wrap gap-2`}>
+                    <img src={channel.avatar} className="w-9 h-9 border rounded-full bg-black" />
+                    <p className="text-white">{video.title}</p>
+                </div>
+                <div className="text-white">
+                    <p>{channel.username}</p>
+                    <div className="flex gap-3">
+                        <p>{video.views} views - {Math.round(num*100)/100} seconds</p>
+                        <p></p>
+                    </div>     
+                </div>
             </div>
-            <div className="text-white">
-                <p>{channel.username}</p>
-                <div className="flex gap-3">
-                    <p>{video.views} views - {Math.round(num*100)/100} seconds</p>
-                    <p></p>
-                </div>     
-            </div>
-        </div>
-       
-        // </Link>
+        )
+       }
+       </>
     )
 }
