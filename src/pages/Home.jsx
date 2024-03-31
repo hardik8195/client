64f182c74add22b3bb664bacb4d7893ac224a120
Hardik gupta <hardikgupta3003@gmail.com>
2024-03-31 20:35:18 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,31 @@ import { useEffect, useState } from "react";
 import Card from "../components/Card";
 import axios from "axios";
 import { BACKEND_URL } from "../utils/http.js";
+import { useSelector } from "react-redux";
 
 
 
 export default function Home({type}){
     const [videos,setVideos] = useState([]);
+    const {user} = useSelector((state)=>state.auth)
 
     useEffect(()=>{
         (async () => {
-            const res = await axios.get(`${BACKEND_URL}/videos/${type}`)
-            setVideos(res.data);
+           try {
+             let config = {}; 
+             if (type === "sub") { 
+                
+                 config = {
+                     headers: {
+                         "Authorization": `Bearer${user.accessToken}`
+                     }
+                 };
+             }
+             const res = await axios.get(`${BACKEND_URL}/videos/${type}`,config);
+             setVideos(res.data);
+           } catch (error) {
+                console.log(error)
+           }
         })()
     },[type,setVideos])
     return (
